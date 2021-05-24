234fbf9f0cb1147262555a06bc8e85b815af9d0e
Pj <pjurad01@ucm.es>
2021-05-24 17:26:27 +0200

    Terminados ejercicios 1, 2 y 3

--- practica2.2/serializacion/ejercicio1.cc
+++ practica2.2/serializacion/ejercicio1.cc
@@ -12,30 +12,42 @@
 class Jugador: public Serializable
 {
 public:
-    Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
+    Jugador(const char * _n, int16_t _x, int16_t _y):pos_x(_x),pos_y(_y)
     {
         strncpy(name, _n, 80);
-    };
+		alloc_data(MAX_NAME * sizeof(char) + 2 * sizeof(int16_t));
+	};
 
-    virtual ~Jugador(){};
+	virtual ~Jugador(){};
 
     void to_bin()
     {
-        //
-    }
+		char *tmp = _data;
+		memcpy(tmp, name, MAX_NAME * sizeof(char));
+		tmp += MAX_NAME * sizeof(char);
+		memcpy(tmp, &pos_x, sizeof(int16_t));
+		tmp += sizeof(int16_t);
+		memcpy(tmp, &pos_y, sizeof(int16_t));
+	}
 
-    int from_bin(char * data)
+	int from_bin(char * data)
     {
-        //
-        return 0;
-    }
-
+		char *tmp = data;
+		memcpy(name, tmp, MAX_NAME * sizeof(char));
+		tmp += MAX_NAME * sizeof(char);
+		memcpy(&pos_x, tmp, sizeof(int16_t));
+		tmp += sizeof(int16_t);
+		memcpy(&pos_y, tmp, sizeof(int16_t));
+		return 0;
+	}
 
 public:
-    char name[80];
+    int16_t pos_x;
+    int16_t pos_y;
 
-    int16_t x;
-    int16_t y;
+	static const size_t MAX_NAME = 80;
+
+	char name[MAX_NAME];
 };
 
 int main(int argc, char **argv)
@@ -43,12 +55,23 @@ int main(int argc, char **argv)
     Jugador one_r("", 0, 0);
     Jugador one_w("Player_ONE", 123, 987);
 
-    // 1. Serializar el objeto one_w
-    // 2. Escribir la serialización en un fichero
-    // 3. Leer el fichero
-    // 4. "Deserializar" en one_r
-    // 5. Mostrar el contenido de one_r
+	int fd;
 
-    return 0;
+	// 1. Serializar el objeto one_w
+	one_w.to_bin();
+	// 2. Escribir la serialización en un fichero
+	fd = open("./jugador", O_CREAT | O_TRUNC | O_RDWR, 0666);
+	write(fd, one_w.data(), one_w.size());
+	close(fd);
+	// 3. Leer el fichero
+	fd = open("./jugador", O_RDONLY);
+	char *buffer = (char*)malloc(one_w.size());
+	read(fd, buffer, one_w.size());
+	close(fd);
+	// 4. "Deserializar" en one_r
+	one_r.from_bin(buffer);
+	// 5. Mostrar el contenido de one_r
+	std::cout << one_r.name << " " << one_r.pos_x << " " << one_r.pos_y << std::endl;
+	return 0;
 }
 
